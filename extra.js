3a33d4cd70b325503dba90dda25dc72d72937c71
Akash Debnath <akash634@gmail.com>
2022-08-26 00:24:56 -0700

    JS practice

--- extra.js
+++ extra.js
@@ -1,8 +1,21 @@
-//   ### DOM selection
+//   ### DOM selection and manipulation
 let myName = document.getElementById('name');
  myName = document.querySelector('#name');
  myName.innerText = "Pulok";
-console.log(myName);
+// console.log(myName);
 
 let designation = document.getElementsByTagName('h3.innerText');
 // console.log(designation);
+
+//  ## addEventListener onclick/onchange
+myName.addEventListener('click', (e)=>{
+    // console.log(e.target);
+});
+
+let darkButton = document.getElementById('dark');
+darkButton.addEventListener('click', (e)=>{
+    document.querySelector('body').innerHTML = "Dark"; //classList.add('bg-dark')
+});
+
+
+// #textNode, mouseevent, appendChild, firstItem
